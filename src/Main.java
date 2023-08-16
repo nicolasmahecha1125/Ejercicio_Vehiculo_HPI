6f4bf355e74974316d2de8a0e42a881a50633b67
nicolas <nicolasmahecha1125@gmail.com>
2023-08-16 00:22:48 -0500

    ssss

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,16 @@ public class Main {
         //creacion de vehiculos
         Automovil carroMazda = new Automovil(4,"Mazda", "TCB-118",4,"gasolina");
         Motocicleta motoHonda = new Motocicleta(2,"Honda","PCP-845","250 cc","Normal");
+        //es conducible
+        carroMazda.conducir();
+        motoHonda.conducir();
         // creacion de propietarios y su asignacion de vehiculos
         Propietario propietario1 = new Propietario("Pedro","Gomez",carroMazda);
         Propietario propietario2 = new Propietario("Nicolas","Santos", motoHonda);
         //mostrar informacion de propietarioa y vehiculos
         propietario1.mostrarInformacion();
         propietario2.mostrarInformacion();
+        
+
     }
 }
